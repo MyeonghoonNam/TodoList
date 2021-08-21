ed441cea68bd07690311c6a1ad5dd5def6786525
MyeongHoonNam <codenmh0822@naver.com>
2021-08-21 19:16:55 +0900

    fix: App Component 예외처리 적용

--- App.js
+++ App.js
@@ -4,25 +4,29 @@ import ToDoForm from './src/ToDoForm.js';
 import { setItem } from './src/Storage.js';
 
 export default function App({ target, initialState }) {
-  new Header({
-    target,
-    text: 'Simple ToDoList !',
-    icon: '<i class="fas fa-list-ul"></i>',
-  });
+  try {
+    new Header({
+      target,
+      text: 'Simple ToDoList !',
+      icon: '<i class="fas fa-list-ul"></i>',
+    });
 
-  const toDoList = new ToDoList({
-    target,
-    initialState,
-  });
+    const toDoList = new ToDoList({
+      target,
+      initialState,
+    });
 
-  new ToDoForm({
-    target,
-    onSubmit: (task) => {
-      const updateState = [...toDoList.state, { task }];
+    new ToDoForm({
+      target,
+      onSubmit: (task) => {
+        const updateState = [...toDoList.state, { task }];
 
-      toDoList.setState(updateState);
+        toDoList.setState(updateState);
 
-      setItem('todos', JSON.stringify(updateState));
-    },
-  });
+        setItem('todos', JSON.stringify(updateState));
+      },
+    });
+  } catch (e) {
+    alert(e.message);
+  }
 }
