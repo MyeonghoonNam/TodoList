d1194f9a9dfc376e2624f920bd2adab66cd38cd2
MyeongHoonNam <codenmh0822@naver.com>
2021-08-21 18:32:17 +0900

    feat: setItem Function

--- src/Storage.js
+++ src/Storage.js
@@ -1,6 +1,12 @@
 const storage = window.localStorage;
 
-export const setItem = (key, value) => {};
+export const setItem = (key, value) => {
+  try {
+    storage.setItem(key, value);
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 export const getItem = (key, defaultValue) => {
   try {
