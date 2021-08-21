53a68e4a9d0c3ceb23577bce3cd216d665c7b27e
MyeongHoonNam <codenmh0822@naver.com>
2021-08-21 18:30:08 +0900

    feat: App Component

---
+++ App.js
@@ -0,0 +1,28 @@
+import Header from './src/Header.js';
+import ToDoList from './src/ToDoList.js';
+import ToDoForm from './src/ToDoForm.js';
+import { setItem } from './src/Storage.js';
+
+export default function App({ target, initialState }) {
+  new Header({
+    target,
+    text: 'Simple ToDoList !',
+    icon: '<i class="fas fa-list-ul"></i>',
+  });
+
+  const toDoList = new ToDoList({
+    target,
+    initialState,
+  });
+
+  new ToDoForm({
+    target,
+    onSubmit: (task) => {
+      const updateState = [...toDoList.state, { task }];
+
+      toDoList.setState(updateState);
+
+      setItem('todos', JSON.stringify(updateState));
+    },
+  });
+}
