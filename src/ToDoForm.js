f8801fc39edaa8596972d6b5e9eb3a4b28c81241
MyeongHoonNam <codenmh0822@naver.com>
2021-08-22 00:43:57 +0900

    repactor: render 함수 flag 제거

--- src/ToDoForm.js
+++ src/ToDoForm.js
@@ -3,27 +3,23 @@ export default function ToDoForm({ target, onSubmit }) {
 
   target.appendChild(toDoForm);
 
-  let isInit = false;
-
   this.render = () => {
     toDoForm.innerHTML = `
       <input type="text" name="todo"></input>
       <button>+ New Task</button>
     `;
 
-    if (!isInit) {
-      toDoForm.addEventListener('submit', (e) => {
-        e.preventDefault();
+    toDoForm.addEventListener('submit', (e) => {
+      e.preventDefault();
 
-        const input = toDoForm.querySelector('input[name=todo]');
-        const task = input.value;
+      const input = toDoForm.querySelector('input[name=todo]');
+      const task = input.value;
 
-        if (task.length > 1) {
-          input.value = '';
-          onSubmit(task);
-        }
-      });
-    }
+      if (task.length > 1) {
+        input.value = '';
+        onSubmit(task);
+      }
+    });
   };
 
   this.render();
